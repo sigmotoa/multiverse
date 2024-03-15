9981b71096888f59ecf06cb261ca39b8895d23ee
sigmotoa <si.galvismotoa@gmail.com>
2024-03-15 07:39:01 -0500

    Adicionar franquicia

--- src/main/java/SuperHero.java
+++ src/main/java/SuperHero.java
@@ -2,7 +2,7 @@ public class SuperHero extends Persona {
 
     String heroName;
     String powers[];
-
+    String franquicia;
     Persona persona;
 
     public SuperHero(String heroName, Persona persona) {
